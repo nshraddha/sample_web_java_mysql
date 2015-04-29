2d2f0ef419883a28e0d4433c3b5b778686914c72
hugo-cloudmunch <hugo@cloudmunch.com>
2015-04-29 13:37:01 -0700

    Testing VS Code Git

--- src/main/java/com/cloudmunch/javademo/Main.java
+++ src/main/java/com/cloudmunch/javademo/Main.java
@@ -1,13 +1,17 @@
-package com.sparkjava.tomcat;
+package com.cloudmunch.javademo;
 
 import static spark.Spark.get;
 import spark.servlet.SparkApplication;
 
 public class Main implements SparkApplication {
-	
+	public static void main(String[] args){
+		SparkApplication app = new Main();
+		app.init();		
+	}
+
 	public void init() {
 		get("/hello", (request, response) -> {
-			return "Ok, Success";
+			return "Hello, World!";
 		});
 	}
 }
