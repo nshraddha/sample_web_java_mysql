1ae042efbd91d9c114977d6c56f29846a3df0f58
cmPrasanna <prasanna@cloudmunch.com>
2015-07-30 09:05:54 +0530

    Update jdk.install.bash

--- sh/jdk.install.bash
+++ sh/jdk.install.bash
@@ -14,7 +14,7 @@ echo "[INFO] Downloading JDK 8"
 
 JDK_RPM_NAME="jdk-8u5-linux-x64.rpm"
 
-wget -c --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-urebackup-cookie" "http://download.oracle.com/otn-p    ub/java/jdk/8u5-b13/jdk-8u5-linux-x64.rpm" --output-document=//$JDK_RPM_NAME
+wget -c --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-urebackup-cookie" "http://download.oracle.com/otn-p    ub/java/jdk/8u5-b13/jdk-8u5-linux-x64.rpm" --output-document=/tmp/$JDK_RPM_NAME
 
 echo "[INFO] Installing RPM"
 sudo rpm -i /tmp/$JDK_RPM_NAME
